c3edefbeb1278a3c7fa0e6182dd3070455e43214
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-01-29 15:35:46 +0200

    fix: add findById to router namespace

--- server/src/controllers/user/index.ts
+++ server/src/controllers/user/index.ts
@@ -1,8 +1,10 @@
 import { router } from '@server/trpc'
 import login from './login'
 import signup from './signup'
+import findById from './findById'
 
 export default router({
   login,
   signup,
+  findById,
 })
