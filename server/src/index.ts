f06920632fcacdf7ad41d27636b1a67c29fb4378
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-01-29 22:28:44 +0200

    fix: create websocket connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,31 @@
+import { createServer } from 'http'
+import { Server as SocketIOServer } from 'socket.io'
 import createApp from './app'
 import { createDatabase } from './database'
 import config from './config'
+import logger from '@server/utils/logger/logger'
 
 const database = createDatabase(config.database)
+
 const app = createApp(database)
 
-app.listen(config.port, () => {
-  // TODO: Add loging here to inform that the server has been launched
-  // eslint-disable-next-line no-console
-  console.log(`Server is running at http://localhost:${config.port}`)
+const httpServer = createServer(app)
+
+export const io = new SocketIOServer(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+})
+
+io.on('connection', (socket) => {
+  logger.info('A client connected via Socket.IO')
+
+  socket.on('disconnect', () => {
+    logger.info('A client disconnected')
+  })
+})
+
+httpServer.listen(config.port, () => {
+  logger.info(`Server is running at http://localhost:${config.port}`)
 })
