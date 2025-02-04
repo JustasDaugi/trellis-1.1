2ac1b35511f29809278f0bc9f095da9006b0c996
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-04 10:58:28 +0200

    refactor: use custom error class

--- server/src/controllers/user/findById.ts
+++ server/src/controllers/user/findById.ts
@@ -2,6 +2,7 @@ import { userSchema, type UserPublic } from '@server/entities/user'
 import provideRepos from '@server/trpc/provideRepos'
 import { userRepository } from '@server/repositories/userRepository'
 import { publicProcedure } from '@server/trpc'
+import NotFoundError from '@server/utils/errors/NotFound'
 
 export default publicProcedure
   .use(
@@ -17,9 +18,13 @@ export default publicProcedure
   .query(async ({ input: { id }, ctx: { repos } }): Promise<UserPublic> => {
     const user = await repos.userRepository.findById(id)
 
+    if (!user) {
+      throw new NotFoundError('User not found')
+    }
+
     return {
-      id: user!.id,
-      firstName: user!.firstName,
-      lastName: user!.lastName,
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
     }
   })
