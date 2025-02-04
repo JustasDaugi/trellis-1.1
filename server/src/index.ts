4e8d58f1c096ad1d5709b89fe4316c37dd90f09d
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-04 11:01:10 +0200

    refactor: import socket setup from socket.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,18 @@
 import { createServer } from 'http'
-import { Server as SocketIOServer } from 'socket.io'
 import createApp from './app'
 import { createDatabase } from './database'
 import config from './config'
 import logger from '@server/utils/logger/logger'
+import { setupSocket } from './socket'
 
 const database = createDatabase(config.database)
 const app = createApp(database)
-
 const httpServer = createServer(app)
 
-export const io = new SocketIOServer(httpServer, {
-  path: '/api/sockets',
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  },
-})
-
-io.on('connection', (socket) => {
-  logger.info('client connected via Socket.IO')
-
-  socket.on('disconnect', () => {
-    logger.info('client disconnected')
-  })
-})
+setupSocket(httpServer)
 
 httpServer.listen(config.port, '0.0.0.0', () => {
   logger.info(`Server is running at http://localhost:${config.port}`)
 })
+
+export default app
