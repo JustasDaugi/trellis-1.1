966a87e269a62600cd718c49d1108cd0bde8cd89
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-05 13:03:11 +0200

    import sendResetLink and resetPassword to router namespace

--- server/src/controllers/user/index.ts
+++ server/src/controllers/user/index.ts
@@ -2,9 +2,13 @@ import { router } from '@server/trpc'
 import login from './login'
 import signup from './signup'
 import findById from './findById'
+import sendResetLink from './sendResetLink'
+import resetPassword from './resetPassword'
 
 export default router({
   login,
   signup,
   findById,
+  sendResetLink,
+  resetPassword,
 })
