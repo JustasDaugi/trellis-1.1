93038e7ccd5bc16934f893949317b42e7c5b7a8a
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-07 14:14:25 +0200

    test uploading images to s3

--- server/src/service/s3/index.ts
+++ server/src/service/s3/index.ts
@@ -1,11 +1,11 @@
-import config from '@server/config'
+import config from '@server/config';
 import {
   S3Client,
   PutObjectCommand,
-  ObjectCannedACL,
   StorageClass,
-} from '@aws-sdk/client-s3'
-import fs from 'fs'
+} from '@aws-sdk/client-s3';
+import fs from 'fs';
+import path from 'path';
 
 const s3 = new S3Client({
   region: config.s3.region,
@@ -13,27 +13,46 @@ const s3 = new S3Client({
     accessKeyId: config.s3.accessKeyId,
     secretAccessKey: config.s3.secretAccessKey,
   },
-})
+});
 
-export async function uploadImage(filePath: string, key: string) {
+export async function uploadImage(filePath: string, key: string): Promise<string> {
   try {
-    const fileStream = fs.createReadStream(filePath)
+    const fileStream = fs.createReadStream(filePath);
 
     const params = {
       Bucket: config.s3.bucketName,
       Key: key,
       Body: fileStream,
       ContentType: 'image/jpg',
-      ACL: ObjectCannedACL.public_read,
       CacheControl: 'max-age=31536000, public',
       StorageClass: StorageClass.STANDARD,
-    }
+    };
 
-    await s3.send(new PutObjectCommand(params))
+    await s3.send(new PutObjectCommand(params));
 
-    return `https://${config.s3.bucketName}.s3.${config.s3.region}.amazonaws.com/${key}`
+    return `https://${config.s3.bucketName}.s3.${config.s3.region}.amazonaws.com/${key}`;
   } catch (error) {
-    console.error('Error uploading image:', error)
-    return ''
+    console.error('Error uploading image:', error);
+    return '';
+  }
+}
+
+export async function uploadAllImages(directory: string): Promise<string[]> {
+  try {
+    const files = await fs.promises.readdir(directory);
+    const imageFiles = files.filter((file) => {
+      const ext = path.extname(file).toLowerCase();
+      return ['.jpg', '.jpeg', '.png'].includes(ext);
+    });
+
+    const uploadPromises = imageFiles.map(async (file) => {
+      const filePath = path.join(directory, file);
+      return await uploadImage(filePath, file);
+    });
+
+    return await Promise.all(uploadPromises);
+  } catch (error) {
+    console.error('Error uploading images from directory:', error);
+    return [];
   }
 }
