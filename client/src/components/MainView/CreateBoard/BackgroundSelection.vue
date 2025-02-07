10317af7f1162d5f0b432890b5e8e03f8df57a5b
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-07 16:10:25 +0200

    s3: replace local image locations with s3 image urls

--- client/src/components/MainView/CreateBoard/BackgroundSelection.vue
+++ client/src/components/MainView/CreateBoard/BackgroundSelection.vue
@@ -1,6 +1,26 @@
 <script setup lang="ts">
 import { defineProps, defineEmits } from 'vue'
 
+const s3BucketUrl = 'https://awsbucket012345.s3.eu-central-1.amazonaws.com'
+
+const backgrounds = [
+  {
+    key: 'background-1.jpg',
+    name: 'Background 1',
+    url: `${s3BucketUrl}/background-1.jpg`,
+  },
+  {
+    key: 'background-2.jpg',
+    name: 'Background 2',
+    url: `${s3BucketUrl}/background-2.jpg`,
+  },
+  {
+    key: 'background-3.jpg',
+    name: 'Background 3',
+    url: `${s3BucketUrl}/background-3.jpg`,
+  },
+]
+
 defineProps<{
   selectedBackground: string
 }>()
@@ -18,66 +38,16 @@ function handleSelect(background: string) {
   <div class="mb-4">
     <h3 class="mb-2 text-sm font-medium text-gray-700">Select Background</h3>
     <div class="flex space-x-2">
-      <div class="relative">
+      <div v-for="bg in backgrounds" :key="bg.key" class="relative">
         <img
-          src="@/assets/background-1.jpg"
-          alt="Background 1"
+          :src="bg.url"
+          :alt="bg.name"
           class="h-16 w-24 cursor-pointer rounded-lg border-2 object-cover"
-          :class="{
-            'border-orchid-500': selectedBackground === '@/assets/background-1.jpg',
-          }"
-          @click="handleSelect('@/assets/background-1.jpg')"
+          :class="{ 'border-orchid-500': selectedBackground === bg.url }"
+          @click="handleSelect(bg.url)"
         />
         <svg
-          v-if="selectedBackground === '@/assets/background-1.jpg'"
-          class="absolute right-1 top-1 h-6 w-6 text-white"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M16.707 5.293a1 1 0 011.414 1.414l-7.364 7.364a1 1 0 01-1.414 0L6.293 11.707a1 1 0 011.414-1.414L10 12.586l5.293-5.293a1 1 0 011.414 0z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </div>
-
-      <div class="relative">
-        <img
-          src="@/assets/background-2.jpg"
-          alt="Background 2"
-          class="h-16 w-24 cursor-pointer rounded-lg border-2 object-cover"
-          :class="{
-            'border-orchid-500': selectedBackground === '@/assets/background-2.jpg',
-          }"
-          @click="handleSelect('@/assets/background-2.jpg')"
-        />
-        <svg
-          v-if="selectedBackground === '@/assets/background-2.jpg'"
-          class="absolute right-1 top-1 h-6 w-6 text-white"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M16.707 5.293a1 1 0 011.414 1.414l-7.364 7.364a1 1 0 01-1.414 0L6.293 11.707a1 1 0 011.414-1.414L10 12.586l5.293-5.293a1 1 0 011.414 0z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </div>
-
-      <div class="relative">
-        <img
-          src="@/assets/background-3.jpg"
-          alt="Background 3"
-          class="h-16 w-24 cursor-pointer rounded-lg border-2 object-cover"
-          :class="{
-            'border-orchid-500': selectedBackground === '@/assets/background-3.jpg',
-          }"
-          @click="handleSelect('@/assets/background-3.jpg')"
-        />
-        <svg
-          v-if="selectedBackground === '@/assets/background-3.jpg'"
+          v-if="selectedBackground === bg.url"
           class="absolute right-1 top-1 h-6 w-6 text-white"
           fill="currentColor"
           viewBox="0 0 20 20"
