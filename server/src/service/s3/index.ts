cde827457b4235e24c35b5e0d718503edaadd406
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-07 16:08:37 +0200

    refactor: remove code to try automated image uploading, keep config parameters for S3 instance

--- server/src/service/s3/index.ts
+++ server/src/service/s3/index.ts
@@ -1,59 +1,19 @@
 import config from '@server/config';
-import {
-  S3Client,
-  PutObjectCommand,
-  StorageClass,
-} from '@aws-sdk/client-s3';
-import fs from 'fs';
-import path from 'path';
+import { S3 } from 'aws-sdk';
 
-const s3 = new S3Client({
+const signedUrlExpireSeconds = 60 * 1;
+
+const s3 = new S3({
+  accessKeyId: config.s3.accessKeyId,
+  secretAccessKey: config.s3.secretAccessKey,
   region: config.s3.region,
-  endpoint: `https://${config.s3.bucketName}.s3.${config.s3.region}.amazonaws.com`,
-  credentials: {
-    accessKeyId: config.s3.accessKeyId,
-    secretAccessKey: config.s3.secretAccessKey,
-  },
+  signatureVersion: 'v4',
 });
 
-export async function uploadImage(filePath: string, key: string): Promise<string> {
-  try {
-    const fileStream = fs.createReadStream(filePath);
-
-    const params = {
-      Bucket: config.s3.bucketName,
-      Key: key,
-      Body: fileStream,
-      ContentType: 'image/jpg',
-      CacheControl: 'max-age=31536000, public',
-      StorageClass: StorageClass.STANDARD,
-    };
-
-    await s3.send(new PutObjectCommand(params));
-
-    return `https://${config.s3.bucketName}.s3.${config.s3.region}.amazonaws.com/${key}`;
-  } catch (error) {
-    console.error('Error uploading image:', error);
-    return '';
-  }
-}
-
-export async function uploadAllImages(directory: string): Promise<string[]> {
-  try {
-    const files = await fs.promises.readdir(directory);
-    const imageFiles = files.filter((file) => {
-      const ext = path.extname(file).toLowerCase();
-      return ['.jpg', '.jpeg', '.png'].includes(ext);
-    });
-
-    const uploadPromises = imageFiles.map(async (file) => {
-      const filePath = path.join(directory, file);
-      return await uploadImage(filePath, file);
-    });
-
-    return await Promise.all(uploadPromises);
-  } catch (error) {
-    console.error('Error uploading images from directory:', error);
-    return [];
-  }
-}
+export const getSignedUrl = (key: string, expires: number = signedUrlExpireSeconds): string => {
+  return s3.getSignedUrl('getObject', {
+    Bucket: config.s3.bucketName,
+    Key: key,
+    Expires: expires,
+  });
+};
