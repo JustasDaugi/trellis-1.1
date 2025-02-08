1b84ea2300d39fa8fe0529f752a9c1b24cee2182
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-08 23:01:47 +0200

    test redis connection

--- server/src/service/redis/index.ts
+++ server/src/service/redis/index.ts
@@ -40,3 +40,50 @@ export const setCache = async (
     console.error('Error setting cache:', error);
   }
 };
+
+
+
+
+
+
+
+// import { createClient } from 'redis'
+
+// const redisHost = process.env.REDIS_HOST
+
+// const redisClient = createClient({
+//   socket: {
+//     host: redisHost,
+//     port: 6379, // non-TLS port
+//   },
+// })
+
+// redisClient.on('error', (err) => {
+//   console.error('Redis error:', err)
+// })
+
+// redisClient.connect().catch(console.error)
+
+// export const getCache = async <T>(key: string): Promise<T | null> => {
+//   try {
+//     const data = await redisClient.get(key)
+//     return data ? (JSON.parse(data) as T) : null
+//   } catch (error) {
+//     console.error('Error retrieving cache:', error)
+//     return null
+//   }
+// }
+
+// export const setCache = async (
+//   key: string,
+//   value: any,
+//   ttl: number
+// ): Promise<void> => {
+//   try {
+//     await redisClient.setEx(key, ttl, JSON.stringify(value))
+//   } catch (error) {
+//     console.error('Error setting cache:', error)
+//   }
+// }
+
+
