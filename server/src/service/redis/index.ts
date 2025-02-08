67196794cdfabde96dde11765e19ff8275af2739
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-09 01:19:59 +0200

    fix: replace console logs with logger

--- server/src/service/redis/index.ts
+++ server/src/service/redis/index.ts
@@ -1,4 +1,5 @@
 import { createClient } from 'redis';
+import logger from '@server/utils/logger/logger';
 
 const redisHost = process.env.REDIS_HOST || '127.0.0.1';
 const redisPort = process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379;
@@ -10,13 +11,16 @@ const redisClient = createClient({
   },
 });
 
-
+redisClient.on('error', (err) => {
+  logger.error('error', `Redis error: ${err}`);
+});
 
 export const getCache = async <T>(key: string): Promise<T | null> => {
   try {
     const data = await redisClient.get(key);
     return data ? (JSON.parse(data) as T) : null;
   } catch (error) {
+    logger.error('error', `Error retrieving cache for key ${key}: ${error}`);
     return null;
   }
 };
@@ -25,6 +29,6 @@ export const setCache = async (key: string, value: any, ttl: number): Promise<vo
   try {
     await redisClient.setEx(key, ttl, JSON.stringify(value));
   } catch (error) {
+    logger.error('error', `Error setting cache for key ${key}: ${error}`);
   }
 };
-
