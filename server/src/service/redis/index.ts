a2990811dfeca78ec45b695c3d638a86a88c8b74
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-08 20:06:39 +0200

    fix: add 'redis' as fallback to redis_host

--- server/src/service/redis/index.ts
+++ server/src/service/redis/index.ts
@@ -1,23 +1,23 @@
 import { createClient } from 'redis'
 
-const redisHost = process.env.REDIS_HOST
+const redisHost = process.env.REDIS_HOST || 'redis'
 
-const redisClient = createClient({
+const pubClient = createClient({
   socket: {
     host: redisHost,
     port: 6379,
   },
 })
 
-redisClient.on('error', (err) => {
+pubClient.on('error', (err) => {
   console.error('Redis error:', err)
 })
 
-redisClient.connect().catch(console.error)
+pubClient.connect().catch(console.error)
 
 export const getCache = async <T>(key: string): Promise<T | null> => {
   try {
-    const data = await redisClient.get(key)
+    const data = await pubClient.get(key)
     return data ? (JSON.parse(data) as T) : null
   } catch (error) {
     console.error('Error retrieving cache:', error)
@@ -25,19 +25,13 @@ export const getCache = async <T>(key: string): Promise<T | null> => {
   }
 }
 
-/**
- * Sets a value in the Redis cache with a given TTL.
- * @param key - Cache key.
- * @param value - Value to cache.
- * @param ttl - Time-to-live in seconds.
- */
 export const setCache = async (
   key: string,
   value: any,
   ttl: number
 ): Promise<void> => {
   try {
-    await redisClient.setEx(key, ttl, JSON.stringify(value))
+    await pubClient.setEx(key, ttl, JSON.stringify(value))
   } catch (error) {
     console.error('Error setting cache:', error)
   }
