8508e209c0fb4b30b3617310dd64729e2957b92c
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-11 00:40:42 +0200

    replace console logs/errors with logger info/error logs

--- server/src/service/redis/index.ts
+++ server/src/service/redis/index.ts
@@ -1,28 +1,24 @@
-import { Redis } from '@upstash/redis';
-import config from '@server/config';
+import { Redis } from '@upstash/redis'
+import config from '@server/config'
+import logger from '@server/utils/logger/logger'
 
 const redis = new Redis({
   url: config.redis.url,
   token: config.redis.token,
-});
+})
 
 export const getCache = async <T>(key: string): Promise<T | null> => {
   try {
-    const data = await redis.get(key);
+    const data = await redis.get(key)
     if (data) {
-      // eslint-disable-next-line no-console
-      console.log(`Cache hit for key: ${key}`);
-      return JSON.parse(data as string) as T;
+      return JSON.parse(data as string) as T
     }
-    // eslint-disable-next-line no-console
-    console.log(`Cache miss for key: ${key}`);
-    return null;
+    return null
   } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('Error retrieving cache:', error);
-    return null;
+    logger.error('Error retrieving cache:', error)
+    return null
   }
-};
+}
 
 export const setCache = async (
   key: string,
@@ -30,11 +26,9 @@ export const setCache = async (
   ttl: number
 ): Promise<void> => {
   try {
-    await redis.set(key, JSON.stringify(value), { ex: ttl });
-    // eslint-disable-next-line no-console
-    console.log(`Cache set for key: ${key} with TTL: ${ttl}`);
+    await redis.set(key, JSON.stringify(value), { ex: ttl })
+    logger.info(`Cache set for key: ${key} with TTL: ${ttl}`)
   } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('Error setting cache:', error);
+    logger.error('Error setting cache:', error)
   }
-};
+}
