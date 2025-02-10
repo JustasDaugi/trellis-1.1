58e62001ce1bf682516d29a3f6bf7c19ce17aa67
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-10 11:28:00 +0200

    refactor: remove logic for fetching linking images with their db references locally

--- client/src/utils/fetchImage.ts
+++ client/src/utils/fetchImage.ts
@@ -1,26 +1,11 @@
 import { computed, type Ref } from 'vue'
 import type { BoardPublic } from '@server/shared/types'
 
-const images = import.meta.glob('@/assets/*.jpg', { eager: true })
-
-const backgroundImages: Record<string, string> = {}
-for (const path in images) {
-  const imageName = path.split('/').pop()!
-  backgroundImages[imageName] = (images[path] as any).default
-}
-
 export function getBackgroundImageUrl(board: BoardPublic): string {
-  if (!board.selectedBackground) return ''
-  const imageName = board.selectedBackground.split('/').pop()!
-  return backgroundImages[imageName] || ''
+  return board.selectedBackground || ''
 }
 
 export function useBackgroundImage(board: Ref<BoardPublic | null>) {
-  const backgroundImageUrl = computed(() => {
-    if (!board.value || !board.value.selectedBackground) return ''
-    const imageName = board.value.selectedBackground.split('/').pop()!
-    return backgroundImages[imageName] || ''
-  })
-
+  const backgroundImageUrl = computed(() => board.value?.selectedBackground || '')
   return { backgroundImageUrl }
 }
