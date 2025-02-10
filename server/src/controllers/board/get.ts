f2d1c05dc312d186c5bf56df047de9abab8972da
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-10 12:27:17 +0200

    feat: add caching for board/get.ts, add console logs for debugging

--- server/src/controllers/board/get.ts
+++ server/src/controllers/board/get.ts
@@ -3,6 +3,7 @@ import { boardRepository } from '@server/repositories/boardRepository'
 import { publicProcedure } from '@server/trpc'
 import provideRepos from '@server/trpc/provideRepos'
 import NotFoundError from '@server/utils/errors/NotFound'
+import { getCache, setCache } from '@server/service/redis'
 
 export default publicProcedure
   .use(
@@ -12,16 +13,37 @@ export default publicProcedure
   )
   .input(idSchema)
   .query(async ({ input: boardId, ctx: { repos } }) => {
-    const board = await repos.boardRepository.findById(boardId)
+    const cacheKey = `board:${boardId}`
 
+    let cachedBoard
+    try {
+      cachedBoard = await getCache<any>(cacheKey)
+      if (cachedBoard) {
+        return cachedBoard
+      }
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(`Error retrieving cache for key ${cacheKey}: ${error}`)
+    }
+
+    const board = await repos.boardRepository.findById(boardId)
     if (!board) {
       throw new NotFoundError('Board not found')
     }
 
-    const selectedBackground = await repos.boardRepository.findSelectedBackground(boardId)
-
-    return {
+    const selectedBackground =
+      await repos.boardRepository.findSelectedBackground(boardId)
+    const boardData = {
       ...board,
       selectedBackground: selectedBackground?.selectedBackground ?? null,
     }
+
+    try {
+      await setCache(cacheKey, boardData, 60)
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(`Error setting cache for key ${cacheKey}: ${error}`)
+    }
+
+    return boardData
   })
