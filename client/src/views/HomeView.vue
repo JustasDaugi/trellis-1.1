6f3e0fe64b9c7f93e6b87a767e751235261de981
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-10 11:54:21 +0200

    feat: add home page image and label

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,12 +1,22 @@
 <script lang="ts" setup>
 import { FwbButton } from 'flowbite-vue'
 import { isLoggedIn } from '@/stores/user'
+
+const homePageImg = 'https://awsbucket012345.s3.eu-central-1.amazonaws.com/home_image.jpg'
 </script>
 
 <template>
   <div class="flex min-h-screen items-center justify-center dark:bg-gray-800">
     <div v-if="!isLoggedIn" class="rounded-md bg-white px-6 py-8">
       <div class="flex flex-col items-center">
+        <div class="relative mb-6" style="width: 270px; height: 300px">
+          <img :src="homePageImg" alt="Home Page" class="h-full w-full object-cover" />
+          <div
+            class="absolute inset-0"
+            style="background: linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.2))"
+          ></div>
+        </div>
+        <p class="mb-6 text-center italic">Collaborate anywhere, anytime</p>
         <div class="mb-40 mt-6 flex flex-col items-center gap-2">
           <FwbButton
             class="w-72 text-center"
@@ -29,10 +39,8 @@ import { isLoggedIn } from '@/stores/user'
           </FwbButton>
         </div>
       </div>
-
       <div class="mt-8 flex items-center justify-center">
-        <div class="max-w-lg">
-        </div>
+        <div class="max-w-lg"></div>
       </div>
     </div>
   </div>
