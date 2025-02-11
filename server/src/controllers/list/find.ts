850077f5238549e54e1eb851c9bff28f7635b9ff
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-11 13:18:53 +0200

    feat: add caching for list/find.ts

--- server/src/controllers/list/find.ts
+++ server/src/controllers/list/find.ts
@@ -2,6 +2,8 @@ import { listSchema, type ListPublic } from '@server/entities/list'
 import provideRepos from '@server/trpc/provideRepos'
 import { listRepository } from '@server/repositories/listRepository'
 import { publicProcedure } from '@server/trpc'
+import { getCache, setCache } from '@server/service/redis'
+import logger from '@server/utils/logger/logger'
 
 export default publicProcedure
   .use(
@@ -16,7 +18,24 @@ export default publicProcedure
   )
   .mutation(
     async ({ input: { boardId }, ctx: { repos } }): Promise<ListPublic[]> => {
+      const cacheKey = `lists:board:${boardId}`
+
+      try {
+        const cachedLists = await getCache<ListPublic[]>(cacheKey)
+        if (cachedLists && Array.isArray(cachedLists)) {
+          return cachedLists
+        }
+      } catch (error) {
+        logger.error('Error retrieving cache for key:', cacheKey, error)
+      }
+
       const lists = await repos.listRepository.findByBoardId(boardId)
+
+      try {
+        await setCache(cacheKey, lists, 60)
+      } catch (error) {
+        logger.error('Error setting cache for key:', cacheKey, error)
+      }
 
       return lists
     }
