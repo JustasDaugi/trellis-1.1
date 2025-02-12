d147db70e099e5f02e34eb6ac7d90d937ae4e29a
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-12 11:37:16 +0200

    redis: add caching for card/find, card/getDueDate endpoints

--- server/src/controllers/card/find.ts
+++ server/src/controllers/card/find.ts
@@ -2,6 +2,8 @@ import { cardSchema, type CardPublic } from '@server/entities/card'
 import provideRepos from '@server/trpc/provideRepos'
 import { cardRepository } from '@server/repositories/cardRepository'
 import { publicProcedure } from '@server/trpc'
+import { getCache, setCache } from '@server/service/redis'
+import logger from '@server/utils/logger/logger'
 
 export default publicProcedure
   .use(
@@ -10,14 +12,38 @@ export default publicProcedure
     })
   )
   .input(
-    cardSchema.pick({
-      listId: true,
-      description: true
-    }).partial({ description: true})
+    cardSchema
+      .pick({
+        listId: true,
+        description: true,
+      })
+      .partial({ description: true })
   )
   .mutation(
     async ({ input: { listId }, ctx: { repos } }): Promise<CardPublic[]> => {
+      const cacheKey = `cards:list:${listId}`
+
+      try {
+        const EXTEND_TTL_THRESHOLD = 4 * 60
+        const cachedCards = await getCache<CardPublic[]>(
+          cacheKey,
+          EXTEND_TTL_THRESHOLD
+        )
+        if (cachedCards && Array.isArray(cachedCards)) {
+          return cachedCards
+        }
+      } catch (error) {
+        logger.error('Error retrieving cache for key:', cacheKey, error)
+      }
+
       const cards = await repos.cardRepository.findByListId(listId)
+
+      try {
+        const CACHE_TTL = 5 * 60
+        await setCache(cacheKey, cards, CACHE_TTL)
+      } catch (error) {
+        logger.error('Error setting cache for key:', cacheKey, error)
+      }
 
       return cards
     }
