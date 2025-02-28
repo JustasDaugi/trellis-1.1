797cb24761f3bf418f7f4a5850443dfbac0a8c5d
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-28 17:14:52 +0200

    router: colocate route definitions with auth requirements

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,88 +1,90 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteLocationNormalized } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import { isLoggedIn } from '@/stores/user'
 
+const routes = [
+  {
+    path: '/dashboard',
+    name: 'MainView',
+    component: () => import('../views/MainView.vue'),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/LoginView.vue'),
+    meta: { guest: true },
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: () => import('../views/SignupView.vue'),
+    meta: { guest: true },
+  },
+  {
+    path: '/reset-password',
+    name: 'SendResetView',
+    component: () => import('../views/SendResetView.vue'),
+    meta: { guest: true },
+  },
+  {
+    path: '/reset-password/confirm',
+    name: 'ResetPasswordView',
+    component: () => import('../views/ResetPasswordView.vue'),
+    props: (route: RouteLocationNormalized) => ({ token: route.query.token }),
+    meta: { guest: true },
+  },
+  {
+    path: '/board/:id',
+    name: 'Board',
+    component: () => import('../views/BoardView.vue'),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/create-board',
+    name: 'CreateBoard',
+    component: () => import('../components/MainView/CreateBoard/CreateBoard.vue'),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/templates',
+    name: 'TemplateView',
+    component: () => import('../views/TemplateView.vue'),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/templates/board/:id',
+    name: 'TemplateBoard',
+    component: () => import('../views/TemplateBoardView.vue'),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView,
+    meta: { guest: true },
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/dashboard',
-      name: 'MainView',
-      component: () => import('../views/MainView.vue'),
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: () => import('../views/LoginView.vue'),
-    },
-    {
-      path: '/signup',
-      name: 'Signup',
-      component: () => import('../views/SignupView.vue'),
-    },
-    {
-      path: '/reset-password',
-      name: 'SendResetView',
-      component: () => import('../views/SendResetView.vue'),
-    },
-    {
-      path: '/reset-password/confirm',
-      name: 'ResetPasswordView',
-      component: () => import('../views/ResetPasswordView.vue'),
-      props: (route) => ({ token: route.query.token }),
-    },
-    {
-      path: '/board/:id',
-      name: 'Board',
-      component: () => import('../views/BoardView.vue'),
-    },
-    {
-      path: '/create-board',
-      name: 'CreateBoard',
-      component: () => import('../components/MainView/CreateBoard/CreateBoard.vue'),
-    },
-    {
-      path: '/templates',
-      name: 'TemplateView',
-      component: () => import('../views/TemplateView.vue'),
-    },
-    {
-      path: '/templates/board/:id',
-      name: 'TemplateBoard',
-      component: () => import('../views/TemplateBoardView.vue'),
-    },
-    {
-      path: '/',
-      name: 'Home',
-      component: HomeView,
-    },
-  ],
+  routes,
 })
 
 router.beforeEach((to, from, next) => {
-  if (isLoggedIn.value) {
-    if (to.name === 'Login' || to.name === 'Signup' || to.name === 'Home') {
-      if (to.query.redirect) {
-        next(to.query.redirect as string)
-      } else {
-        next({ name: 'MainView' })
-      }
+  if (to.meta.requiresAuth && !isLoggedIn.value) {
+    return next({ name: 'Login', query: { redirect: to.fullPath } })
+  }
+
+  if (to.meta.guest && isLoggedIn.value) {
+    if (to.query.redirect) {
+      return next(to.query.redirect as string)
     } else {
-      next()
-    }
-  } else {
-    if (
-      to.name === 'MainView' ||
-      to.path.startsWith('/dashboard') ||
-      to.name === 'Board' ||
-      to.name === 'TemplateView' ||
-      to.name === 'TemplateBoard'
-    ) {
-      next({ name: 'Login', query: { redirect: to.fullPath } })
-    } else {
-      next()
+      return next({ name: 'MainView' })
     }
   }
+
+  next()
 })
 
 export default router
