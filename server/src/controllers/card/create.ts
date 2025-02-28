87fcd98872850e6ccb20622b86b76cf17d6019e9
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-28 13:14:52 +0200

    fix: allow only board members/owners to create cards on lists in a board

--- server/src/controllers/card/create.ts
+++ server/src/controllers/card/create.ts
@@ -2,14 +2,19 @@ import { cardSchema, type CardPublic } from '@server/entities/card'
 import provideRepos from '@server/trpc/provideRepos'
 import { listRepository } from '@server/repositories/listRepository'
 import { cardRepository } from '@server/repositories/cardRepository'
-import { publicProcedure } from '@server/trpc'
+import { boardMemberRepository } from '@server/repositories/boardMemberRepository'
+import { boardRepository } from '@server/repositories/boardRepository'
+import { authenticatedProcedure } from '@server/trpc/authenticatedProcedure'
 import NotFoundError from '@server/utils/errors/NotFound'
+import ForbiddenError from '@server/utils/errors/Forbidden'
 
-export default publicProcedure
+export default authenticatedProcedure
   .use(
     provideRepos({
       listRepository,
       cardRepository,
+      boardRepository,
+      boardMemberRepository,
     })
   )
   .input(
@@ -17,19 +22,41 @@ export default publicProcedure
       .pick({
         listId: true,
         title: true,
-        userId: true,
         description: true,
       })
       .partial({ description: true })
   )
-  .mutation(async ({ input: card, ctx: { repos } }): Promise<CardPublic> => {
-    const list = await repos.listRepository.findById(card.listId)
+  .mutation(
+    async ({ input, ctx: { repos, authUser } }): Promise<CardPublic> => {
+      const { listId, title, description } = input
+      const list = await repos.listRepository.findById(listId)
+      if (!list) {
+        throw new NotFoundError('List not found.')
+      }
 
-    if (!list) {
-      throw new NotFoundError('Board not found')
+      const board = await repos.boardRepository.findById(list.boardId)
+      if (!board) {
+        throw new NotFoundError(`Board with id ${list.boardId} not found.`)
+      }
+
+      const isOwner = board.userId === authUser.id
+      const isMember = await repos.boardMemberRepository.isMemberOfBoard(
+        board.id,
+        authUser.id
+      )
+      if (!isOwner && !isMember) {
+        throw new ForbiddenError(
+          'You are not authorized to create a card in this board.'
+        )
+      }
+
+      const newCard = await repos.cardRepository.create({
+        listId,
+        title,
+        description: description || null,
+        userId: authUser.id,
+      })
+
+      return newCard
     }
-
-    const newCard = await repos.cardRepository.create(card)
-
-    return newCard
-  })
+  )
