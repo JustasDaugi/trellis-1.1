4cbf29bd561beec19fe268cf241bac0f8f101b6e
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-28 13:13:13 +0200

    fix: remove caching

--- server/src/controllers/card/getDueDate.ts
+++ server/src/controllers/card/getDueDate.ts
@@ -2,7 +2,6 @@ import { cardSchema } from '@server/entities/card'
 import provideRepos from '@server/trpc/provideRepos'
 import { cardRepository } from '@server/repositories/cardRepository'
 import { publicProcedure } from '@server/trpc'
-import { getCache, setCache } from '@server/service/redis'
 import logger from '@server/utils/logger/logger'
 
 export default publicProcedure
@@ -17,29 +16,11 @@ export default publicProcedure
     })
   )
   .query(async ({ input: { id }, ctx: { repos } }): Promise<Date | null> => {
-    const cacheKey = `card:dueDate:${id}`
-
     try {
-      const EXTEND_TTL_THRESHOLD = 4 * 60
-      const cachedDueDate = await getCache<Date | null>(
-        cacheKey,
-        EXTEND_TTL_THRESHOLD
-      )
-      if (cachedDueDate) {
-        return cachedDueDate
-      }
+      const dueDate = await repos.cardRepository.getDueDate(id)
+      return dueDate
     } catch (error) {
-      logger.error('Error retrieving cache for key:', cacheKey, error)
+      logger.error('Error retrieving due date for card:', id, error)
+      throw error
     }
-
-    const dueDate = await repos.cardRepository.getDueDate(id)
-
-    try {
-      const CACHE_TTL = 5 * 60
-      await setCache(cacheKey, dueDate, CACHE_TTL)
-    } catch (error) {
-      logger.error('Error setting cache for key:', cacheKey, error)
-    }
-
-    return dueDate
   })
