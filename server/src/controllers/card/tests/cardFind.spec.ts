47dc200def5bc04d7bc9c3443484292f1b54c297
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-28 13:16:21 +0200

    fix: add test to check if 403 is thrown when users is neither member nor owner

--- server/src/controllers/card/tests/cardFind.spec.ts
+++ server/src/controllers/card/tests/cardFind.spec.ts
@@ -1,10 +1,12 @@
 import { authRepoContext } from '@tests/utils/context'
 import { createCallerFactory } from '@server/trpc'
-import type { CardRepository } from '@server/repositories/cardRepository'
 import { fakeCard } from '@server/entities/tests/fakes'
 import cardRouter from '..'
 
-const repos = {
+const userId = 123
+const boardId = 999
+
+const commonRepos = {
   cardRepository: {
     findByListId: async (listId: number) => [
       fakeCard({
@@ -15,20 +17,42 @@ const repos = {
         dueDate: null,
       }),
     ],
-  } satisfies Partial<CardRepository>,
+  },
+  listRepository: {
+    findById: async (listId: number) => {
+      return {
+        id: listId,
+        boardId,
+      }
+    },
+  },
+  boardRepository: {
+    findById: async (id: number) => {
+      return {
+        id,
+        userId,
+      }
+    },
+  },
+  boardMemberRepository: {
+    isMemberOfBoard: async () => {
+      return true
+    },
+  },
 }
 
 const createCaller = createCallerFactory(cardRouter)
-const { find } = createCaller(authRepoContext(repos))
+const happyCaller = createCaller(authRepoContext(commonRepos, { id: userId }))
+const { find: findHappy } = happyCaller
 
 it('should return a list of cards for a given list', async () => {
-  // ARRANGE (Given)
+  // ARRANGE
   const listId = 5
 
-  // ACT (When)
-  const cardsFound = await find({ listId })
+  // ACT
+  const cardsFound = await findHappy({ listId })
 
-  // ASSERT (Then)
+  // ASSERT
   expect(cardsFound).toMatchObject([
     {
       listId,
@@ -36,4 +60,33 @@ it('should return a list of cards for a given list', async () => {
       order: 1,
     },
   ])
+})
+
+it('should throw ForbiddenError if user is neither owner nor member', async () => {
+  // ARRANGE:
+
+  const notOwnerOrMemberRepos = {
+    ...commonRepos,
+    boardRepository: {
+      findById: async (id: number) => {
+        return {
+          id,
+          userId: 9999,
+        }
+      },
+    },
+    boardMemberRepository: {
+      isMemberOfBoard: async () => false,
+    },
+  }
+
+  const notMemberCaller = createCaller(authRepoContext(notOwnerOrMemberRepos, { id: 111 }))
+  const { find: findForbidden } = notMemberCaller
+
+  const listId = 5
+
+  // ACT & ASSERT
+  await expect(findForbidden({ listId })).rejects.toThrow(
+    /You do not have permission to view cards on this list/
+  )
 })
