7c03b51c97b8c7b3a1efca61331312838a0846a8
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-28 13:09:25 +0200

    test: add new test to throw 403 if user is neither owner nor member of board

--- server/src/controllers/board/tests/boardGetBoardOwner.spec.ts
+++ server/src/controllers/board/tests/boardGetBoardOwner.spec.ts
@@ -1,37 +1,41 @@
+import { createCallerFactory } from '@server/trpc'
+import boardRouter from '..'
+import { authContext } from '@tests/utils/context'
+import { wrapInRollbacks } from '@tests/utils/transactions'
+import { createTestDatabase } from '@tests/utils/database'
+import { insertAll } from '@tests/utils/records'
 import {
   fakeBoard,
   fakeBoardMember,
   fakeUser,
 } from '@server/entities/tests/fakes'
-import { createTestDatabase } from '@tests/utils/database'
-import { createCallerFactory } from '@server/trpc'
-import { wrapInRollbacks } from '@tests/utils/transactions'
-import { insertAll } from '@tests/utils/records'
-import boardRouter from '..'
 
 const createCaller = createCallerFactory(boardRouter)
 const db = await wrapInRollbacks(createTestDatabase())
 
-const [user1] = await insertAll(db, 'user', [
+const [user1, user2] = await insertAll(db, 'user', [
   fakeUser({
     firstName: 'John',
     lastName: 'Doe',
     email: 'john.doe@example.com',
   }),
+  fakeUser({
+    firstName: 'Jane',
+    lastName: 'Smith',
+    email: 'jane.smith@example.com',
+  }),
 ])
 
 const [board] = await insertAll(db, 'board', [fakeBoard({ userId: user1.id })])
 
-const { getBoardOwner } = createCaller({ db })
+const { getBoardOwner } = createCaller(authContext({ db }, user1))
 
 it('should throw NotFoundError if the board owner does not exist', async () => {
   // ARRANGE
   const nonExistentBoardId = 99999
 
   // ACT & ASSERT
-  await expect(getBoardOwner(nonExistentBoardId)).rejects.toThrow(
-    'Board owner not found'
-  )
+  await expect(getBoardOwner(nonExistentBoardId)).rejects.toThrow(/not found/i)
 })
 
 it('should return the board ID and board owner ID when the board owner exists', async () => {
@@ -52,4 +56,15 @@ it('should return the board ID and board owner ID when the board owner exists', 
     boardId: board.id,
     boardOwnerId: user1.id,
   })
+})
+
+it('should throw ForbiddenError if user is neither owner nor member', async () => {
+  const { getBoardOwner: getBoardOwnerForUser2 } = createCaller(
+    authContext({ db }, user2)
+  )
+
+  // ACT & ASSERT
+  await expect(getBoardOwnerForUser2(board.id)).rejects.toThrow(
+    /Not authorized/i
+  )
 })
