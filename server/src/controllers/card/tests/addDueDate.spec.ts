22ae898a780df2776bcbce2c300427f9c31889f4
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-02-28 16:59:02 +0200

    fix: use authContext for managing permissions to add due dates

--- server/src/controllers/card/tests/addDueDate.spec.ts
+++ server/src/controllers/card/tests/addDueDate.spec.ts
@@ -9,6 +9,7 @@ import { insertAll } from '@tests/utils/records'
 import { wrapInRollbacks } from '@tests/utils/transactions'
 import { createTestDatabase } from '@tests/utils/database'
 import cardRouter from '..'
+import { authContext } from '@tests/utils/context'
 
 const createCaller = createCallerFactory(cardRouter)
 const db = await wrapInRollbacks(createTestDatabase())
@@ -46,8 +47,9 @@ it('throws an error if the card does not exist', async () => {
   const nonExistentCardId = card.id + 999999
   const dueDate = new Date()
 
+  const { addDueDate } = createCaller(authContext({ db }, user))
+
   // ACT & ASSERT
-  const { addDueDate } = createCaller({ db })
   await expect(
     addDueDate({
       id: nonExistentCardId,
@@ -59,7 +61,7 @@ it('throws an error if the card does not exist', async () => {
 describe('permissions', () => {
   it('allows a user to add a due date to a card', async () => {
     // ARRANGE
-    const { addDueDate } = createCaller({ db })
+    const { addDueDate } = createCaller(authContext({ db }, user))
     const dueDate = new Date()
 
     // ACT & ASSERT
