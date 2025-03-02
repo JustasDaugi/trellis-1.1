50c05a5aee1b51b6b9dade4dd9ba4524842b63c3
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-03-02 16:20:56 +0200

    feat: add cache invalidation to middleware

--- server/src/middleware/index.ts
+++ server/src/middleware/index.ts
@@ -1,9 +1,10 @@
-import { getCache, setCache } from '@server/service/redis'
+import { getCache, setCache, invalidateCache } from '@server/service/redis'
 import logger from '@server/utils/logger/logger'
 
 export const cacheMiddleware = (options: {
   key: ({ input, ctx }: { input: unknown; ctx: any }) => string
   ttl: number
+  invalidate?: boolean
 }) => {
   return async ({
     ctx,
@@ -17,30 +18,46 @@ export const cacheMiddleware = (options: {
     const cacheKey = options.key({ input, ctx })
     logger.info(`Cache middleware triggered for key: ${cacheKey}`)
 
-    try {
-      const cachedData = await getCache<any>(cacheKey)
-      if (cachedData) {
-        logger.info(`Cache hit for key ${cacheKey}`)
-        return cachedData
-      } else {
-        logger.info(`Cache miss for key ${cacheKey}`)
+    if (!options.invalidate) {
+      try {
+        const cachedData = await getCache<any>(cacheKey)
+        if (cachedData) {
+          logger.info(`Cache hit for key ${cacheKey}`)
+          return cachedData
+        } else {
+          logger.info(`Cache miss for key ${cacheKey}`)
+        }
+      } catch (error: any) {
+        logger.error(
+          `Error retrieving cache for key ${cacheKey}: ${error?.message || error}`
+        )
       }
-    } catch (error: any) {
-      logger.error(
-        `Error retrieving cache for key ${cacheKey}: ${error?.message || error}`
-      )
     }
 
     const result = await next()
 
-    try {
-      logger.info(`Attempting to cache response for key ${cacheKey}`)
-      await setCache(cacheKey, result, options.ttl)
-      logger.info(`Response cached for key ${cacheKey} with TTL ${options.ttl}`)
-    } catch (error: any) {
-      logger.error(
-        `Error setting cache for key ${cacheKey}: ${error?.message || error}`
-      )
+    if (options.invalidate) {
+      try {
+        logger.info(`Invalidating cache for key ${cacheKey}`)
+        await invalidateCache(cacheKey)
+        logger.info(`Cache invalidated for key ${cacheKey}`)
+      } catch (error: any) {
+        logger.error(
+          `Error invalidating cache for key ${cacheKey}: ${error?.message || error}`
+        )
+      }
+    } else {
+      try {
+        logger.info(`Attempting to cache response for key ${cacheKey}`)
+        await setCache(cacheKey, result, options.ttl)
+        logger.info(
+          `Response cached for key ${cacheKey} with TTL ${options.ttl}`
+        )
+      } catch (error: any) {
+        logger.error(
+          `Error setting cache for key ${cacheKey}: ${error?.message || error}`
+        )
+      }
     }
 
     return result
