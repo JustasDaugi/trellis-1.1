d28ba3184cbeb077cc431c748af59372160a882d
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-03-02 13:33:47 +0200

    cache: use custom middleware for caching

--- server/src/controllers/board/findAll.ts
+++ server/src/controllers/board/findAll.ts
@@ -4,8 +4,13 @@ import { authenticatedProcedure } from '@server/trpc/authenticatedProcedure'
 import provideRepos from '@server/trpc/provideRepos'
 import { boardSchema } from '@server/entities/board'
 import { z } from 'zod'
-import { getCache, setCache } from '@server/service/redis'
-import logger from '@server/utils/logger/logger'
+import { cacheMiddleware } from '@server/middleware'
+
+type BoardFindAllInput = {
+  userId: number
+  limit: number
+  offset: number
+}
 
 export default authenticatedProcedure
   .use(
@@ -15,32 +20,22 @@ export default authenticatedProcedure
     })
   )
   .input(
-    boardSchema
-      .pick({
-        userId: true,
-      })
-      .extend({
-        limit: z.number().int().positive().default(10),
-        offset: z.number().int().nonnegative().default(0),
-      })
+    boardSchema.pick({ userId: true }).extend({
+      limit: z.number().int().positive().default(10),
+      offset: z.number().int().nonnegative().default(0),
+    })
+  )
+  .use(
+    cacheMiddleware({
+      key: ({ input }) => {
+        const typedInput = input as BoardFindAllInput
+        return `limit:${typedInput.limit}:offset:${typedInput.offset}`
+      },
+      ttl: 5 * 60,
+    })
   )
   .query(
     async ({ input: { userId, limit, offset }, ctx: { repos, authUser } }) => {
-      const cacheKey = `boards-allowed:${authUser.id}:user:${userId}:limit:${limit}:offset:${offset}`
-
-      try {
-        const EXTEND_TTL_THRESHOLD = 4 * 60
-        const cachedBoards = await getCache(cacheKey, EXTEND_TTL_THRESHOLD)
-        if (cachedBoards) {
-          return cachedBoards
-        }
-      } catch (error) {
-        logger.error(
-          'error',
-          `Error retrieving cache for key ${cacheKey}: ${error}`
-        )
-      }
-
       const allBoards = await repos.boardRepository.findAllByUserId(
         userId,
         limit,
@@ -59,16 +54,6 @@ export default authenticatedProcedure
       )
 
       const allowedBoards = boardsCheckResults.filter((b) => b !== null)
-      try {
-        const CACHE_TTL = 5 * 60
-        await setCache(cacheKey, allowedBoards, CACHE_TTL)
-      } catch (error) {
-        logger.error(
-          'error',
-          `Error setting cache for key ${cacheKey}: ${error}`
-        )
-      }
-
       return allowedBoards
     }
   )
