fc9c012316ee6e87b5f134a1b71bfe3e25214a95
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-03-02 16:25:16 +0200

    feat: add cache invalidation after boards are deleted

--- server/src/controllers/board/deleteById.ts
+++ server/src/controllers/board/deleteById.ts
@@ -2,21 +2,23 @@ import { boardSchema, type BoardPublic } from '@server/entities/board'
 import { authenticatedProcedure } from '@server/trpc/authenticatedProcedure'
 import provideRepos from '@server/trpc/provideRepos'
 import { boardRepository } from '@server/repositories/boardRepository'
-import NotFoundError from "@server/utils/errors/NotFound";
-import ForbiddenError from "@server/utils/errors/Forbidden";
+import NotFoundError from '@server/utils/errors/NotFound'
+import ForbiddenError from '@server/utils/errors/Forbidden'
+import { cacheMiddleware } from '@server/middleware'
 
 export default authenticatedProcedure
   .use(provideRepos({ boardRepository }))
-  .input(
-    boardSchema.pick({
-      id: true,
+  .input(boardSchema.pick({ id: true }))
+  .use(
+    cacheMiddleware({
+      key: ({ ctx }) =>
+        `boards-allowed:${ctx.authUser.id}:user:${ctx.authUser.id}:limit:10:offset:0`,
+      ttl: 0,
+      invalidate: true,
     })
   )
   .mutation(
-    async ({
-      input: { id },
-      ctx: { repos, authUser },
-    }): Promise<BoardPublic> => {
+    async ({ input: { id }, ctx: { repos, authUser } }): Promise<BoardPublic> => {
       const board = await repos.boardRepository.findById(id)
 
       if (!board) {
