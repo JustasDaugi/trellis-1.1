69fa9555f936e635d0cc534d0d6a6517f79cc76f
JustasDaugi <justas.daugininkaitis@gmail.com>
2025-03-04 14:29:36 +0200

    refactor: replace getCache and setCache functions with middleware for caching

--- server/src/controllers/board/fetchData.ts
+++ server/src/controllers/board/fetchData.ts
@@ -5,8 +5,7 @@ import provideRepos from '@server/trpc/provideRepos'
 import { listRepository } from '@server/repositories/listRepository'
 import { cardRepository } from '@server/repositories/cardRepository'
 import { publicProcedure } from '@server/trpc'
-import { getCache, setCache } from '@server/service/redis'
-import logger from '@server/utils/logger/logger'
+import { cacheMiddleware } from '@server/middleware'
 
 const cardPublicSchema = cardSchema
   .pick({
@@ -42,60 +41,38 @@ export default publicProcedure
     })
   )
   .input(listSchema.pick({ boardId: true }))
+  .use(
+    cacheMiddleware({
+      key: ({ input }) =>
+        `board:fetchData:${(input as { boardId: number }).boardId}`,
+      ttl: 5 * 60,
+    })
+  )
   .query(async ({ input: { boardId }, ctx: { repos } }) => {
-    const cacheKey = `board:fetchData:${boardId}`
+    const lists = await repos.listRepository.findByBoardId(boardId)
 
-    try {
-      const EXTEND_TTL_THRESHOLD = 4 * 60
-      const cachedData = await getCache<z.infer<typeof boardDataSchema>>(
-        cacheKey,
-        EXTEND_TTL_THRESHOLD
-      )
-      if (cachedData) {
-        return cachedData
-      }
-    } catch (error) {
-      logger.error('Error retrieving cache for key:', cacheKey, error)
-    }
-
-    try {
-      const lists = await repos.listRepository.findByBoardId(boardId)
-
-      const listsWithCards = await Promise.all(
-        lists.map(async (list) => {
-          const cards = await repos.cardRepository.findByListId(list.id)
-
-          const cardsWithDueDate = await Promise.all(
-            cards.map(async (card) => {
-              const dueDate = await repos.cardRepository.getDueDate(card.id)
-              return {
-                ...card,
-                dueDate,
-              }
-            })
-          )
-
-          return {
-            ...list,
-            cards: cardsWithDueDate,
-          }
-        })
-      )
-
-      const fullBoardData = boardDataSchema.parse({
-        lists: listsWithCards,
+    const listsWithCards = await Promise.all(
+      lists.map(async (list) => {
+        const cards = await repos.cardRepository.findByListId(list.id)
+        const cardsWithDueDate = await Promise.all(
+          cards.map(async (card) => {
+            const dueDate = await repos.cardRepository.getDueDate(card.id)
+            return {
+              ...card,
+              dueDate,
+            }
+          })
+        )
+        return {
+          ...list,
+          cards: cardsWithDueDate,
+        }
       })
+    )
 
-      try {
-        const CACHE_TTL = 5 * 60
-        await setCache(cacheKey, fullBoardData, CACHE_TTL)
-      } catch (cacheError) {
-        logger.error('Error setting cache for key:', cacheKey, cacheError)
-      }
+    const fullBoardData = boardDataSchema.parse({
+      lists: listsWithCards,
+    })
 
-      return fullBoardData
-    } catch (dbError) {
-      logger.error('Error building board data for boardId:', boardId, dbError)
-      return boardDataSchema.parse({ lists: [] })
-    }
+    return fullBoardData
   })
